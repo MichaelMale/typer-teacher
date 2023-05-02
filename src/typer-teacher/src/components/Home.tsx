d4352e2a4b5be78e1d99a0186a1f5a3c054810a4
Michael Male <michaelmale31@gmail.com>
2023-05-03 00:50:27 +0100

    Reafactor

--- src/typer-teacher/src/components/Home.tsx
+++ src/typer-teacher/src/components/Home.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Home: React.FC = () => {
-    return (
-        <div className="container">
-          <div className="centered-content">
-            <h1 className="text-center">Teacher Typer</h1>
-          </div>
-        </div>
-      );
+const Home = () => {
+  useEffect(() => {
+    document.title = 'Typer Teacher - Home';
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="centered-content">
+        <h1 className="text-center">Typer Teacher</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
